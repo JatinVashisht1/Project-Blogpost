118b2f40ab6ac25d1d48291b6831833315431292
priyanshsingh <priyanshsingh1401@gmail.com>
2022-04-29 17:14:57 +0530

    added frontend code

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+
+import React from 'react';
+
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Contact from "./components/Contact";
+import Home from './components/Home';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// import About from "./components/Contact";
+// import Hello from "./components/material_ui_components/Hello";
+// import Cards from "./components/material_ui_components/Cards/Cards.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar title = "Blog Post" style={{position:'fixed'}}/>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+
+      {/* <Cards /> */}
+
+      {/* <About />
+      <Hello /> */}
+    </>
+  );
+}
+
+export default App;
